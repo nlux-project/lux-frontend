15c8c058cd75c115936435ad2e04898346edf021
Joost Soeterbroek <joost.soeterbroek@gmail.com>
2026-04-27 15:30:29 +0200

    Use neutral related objects message

--- client/src/test/unit/features/common/RelatedObjectsAndWorks.spec.tsx
+++ client/src/test/unit/features/common/RelatedObjectsAndWorks.spec.tsx
@@ -1,10 +1,13 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
+import { AuthProvider } from 'react-oidc-context'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import RelatedObjectsWorksAndCollections from '../../../../features/common/RelatedObjectsWorksAndCollections'
 import { entity as mockEntity } from '../../../data/entity'
 import ILinks from '../../../../types/data/ILinks'
+import { store } from '../../../../app/store'
 
 const mockRelationships = {
   objects: {
@@ -17,18 +20,28 @@ const mockRelationships = {
 describe('RelatedObjectsWorksAndCollections', () => {
   it('renders the error message', async () => {
     render(
-      <BrowserRouter>
-        <RelatedObjectsWorksAndCollections
-          links={mockEntity._links as ILinks}
-          relationships={mockRelationships}
-          type="event"
-        />
-      </BrowserRouter>,
+      <Provider store={store}>
+        <BrowserRouter>
+          <AuthProvider
+            authority="dummy"
+            client_id="dummy"
+            redirect_uri="dummy"
+            response_type="code"
+            scope="openid email"
+          >
+            <RelatedObjectsWorksAndCollections
+              links={mockEntity._links as ILinks}
+              relationships={mockRelationships}
+              type="event"
+            />
+          </AuthProvider>
+        </BrowserRouter>
+      </Provider>,
     )
 
     const message = screen.getByTestId('no-related-objects-works')
     expect(message).toHaveTextContent(
-      'Yale University does not have any objects or works directly related to this event.',
+      'We do not have any objects or works directly related to this event.',
     )
   })
 })
